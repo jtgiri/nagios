ce3eeed355b48b3859d7d00c90d257515dd95f59
Abhijit Vaidya <abhijit@nclouds.com>
2014-02-12 14:16:07 +0530

    adding nagios and log config, added recipe for gearman and nagios

--- recipes/nag-mod-server.rb
+++ recipes/nag-mod-server.rb
@@ -1,10 +1,16 @@
+case node["platform"]
+when "debian", "ubuntu"
 include_recipe "apt"
+include_recipe "nagios::server"
 
+# Installing packges
 %w{gearman-job-server gearman-tools mod-gearman-module mod-gearman-tools mod-gearman-worker}.each do |pkg|
   package pkg do
   action :install
   end
 end
+
+# Creating Mod-gearman Config packages
 
 template "/etc/mod-gearman/mod_gearman.cfg" do
   source "mod_gearman.cfg.erb"
@@ -26,10 +32,34 @@ template "/etc/default/gearman-job-server" do
   action :create
 end
 
+# Adding nagios config with mod-gearman
+
+template "/etc/nagios3/nagios.cfg"
+  source "nagios-mod.cfg.erb"
+  action :create
+end
+
+# Creating Log dir
+directory "/var/log/mod-gearman" do
+  owner 'nagios'
+  group 'nagios'
+  action :create
+end
+
+# Restarting services
+service "nagios3" do
+ action [:enable, :restart]
+end
+
 service "gearman-job-server" do
   action [:enable, :restart]
 end
 
 service "mod-gearman-worker" do
   action [:enable, :restart]
+end
+
+else
+Chef::Log.fatal('Platform is not debian or ubuntu')
+fail
 end
