815b31b7651d3cb782ee7e494adc9b12f46bca08
Abhijit Vaidya <abhijit@nclouds.com>
2014-02-20 00:02:54 +0530

    adding more packages

--- recipes/worker.rb
+++ recipes/worker.rb
@@ -1,4 +1,4 @@
- %w{ mod-gearman-tools mod-gearman-worker}.each do |pkg|
+ %w{mod-gearman-tools mod-gearman-worker libnagios-plugin-perl nagios3-core nagios3-common nagios-nrpe-plugin}.each do |pkg|
    package pkg do
     action :install
    end
